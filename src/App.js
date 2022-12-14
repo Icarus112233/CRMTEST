94ed44e98325ba4e7d5bdf88deab0e685ab46917
Farooq, Salman <salmanfarooq@my.unt.edu>
2022-12-14 17:15:43 -0600

    project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './styles/App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Customers from './pages/Customers';
+import CustomerList from './pages/CustomerList';
+import CreateTask from './pages/CreateTask';
+import TaskList from './pages/TaskList';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="wrapper" className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/createCustomer' element={<Customers />} />
+          <Route path='/customerList' element={<CustomerList />} />
+          <Route path='/createTask' element={<CreateTask />}/> 
+          {/* We can make tasks list it's own page if we want
+              I have commented it out here and in the Navbar index
+           <Route path='/taskList' element={<TaskList />}/> */
+          }
+        </Routes>
+      </Router>
     </div>
+    
   );
 }
 
